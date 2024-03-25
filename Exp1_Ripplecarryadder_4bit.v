b38e2a8fb856864405846d87e7c7674cac93b00a
Christina1106 <161043650+Christina1106@users.noreply.github.com>
2024-03-24 22:54:08 -0700

    Update Exp1_Ripplecarryadder_4bit.v

--- Exp1_Ripplecarryadder_4bit.v
+++ Exp1_Ripplecarryadder_4bit.v
@@ -8,3 +8,14 @@ fulladder u1 (S[0], w1,X[0], Y[0], Cin);
 fulladder u2(S[1], w2,X[1], Y[1], w1);
 fulladder u3(S[2], w3,X[2], Y[2], w2); fulladder u4(S[3], Cout, X[3], Y[3], w3);
 endmodule
+
+module fulladder(S, Co, X, Y, Ci);
+input X, Y, Ci;
+output S, Co;
+wire w1,w2,w3;
+xor G1(w1, X, Y);
+xor G2(S, w1, Cí);
+and G3(w2, w1, Ci);
+and G4(w3, X, Y);
+or G5(Co, w2, w3);
+endmodule
